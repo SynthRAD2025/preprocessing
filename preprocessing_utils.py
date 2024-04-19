92048efbe902e6b494b681e8aaeb05ea6951766a
Adrian Thummerer <adrian.thummerer@med.uni-muenchen.de>
2024-04-19 21:54:54 +0000

    added docstrings

--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -7,10 +7,29 @@ from scipy.signal import find_peaks
 from totalsegmentator.python_api import totalsegmentator
 
 def read_image(image_path:str)->sitk.Image:
+    """
+    Read an image from the specified image path using SimpleITK.
+
+    Parameters:
+    image_path (str): The path to the image file. All ITK file formats can be loaded.
+
+    Returns:
+    sitk.Image: The loaded image.
+
+    """
     image = sitk.ReadImage(image_path)
     return image
 
 def read_dicom_image(image_path:str)->sitk.Image:
+    """
+    Reads a DICOM image from the specified image path using SimpleITK.
+
+    Parameters:
+    image_path (str): The path to the DICOM image.
+
+    Returns:
+    sitk.Image: The loaded DICOM image.
+    """
     reader = sitk.ImageSeriesReader()
     dicom_names = reader.GetGDCMSeriesFileNames(image_path)
     reader.SetFileNames(dicom_names)
@@ -18,9 +37,26 @@ def read_dicom_image(image_path:str)->sitk.Image:
     return image
 
 def save_image(image:sitk.Image, image_path:str):
-    sitk.WriteImage(image, image_path,useCompression=True)
+    """
+    Save the given SimpleITK image to the specified file path.
+    
+    Args:
+        image (sitk.Image): The SimpleITK image to be saved.
+        image_path (str): The file path where the image will be saved.
+    """
+    sitk.WriteImage(image, image_path, useCompression=True)
 
 def convert_rtstruct_to_nrrd(rtstruct_path:str, nrrd_dir_path:str):
+    """
+    Converts an RTSTRUCT file to NRRD format.
+
+    Parameters:
+    rtstruct_path (str): The path to the RTSTRUCT file.
+    nrrd_dir_path (str): The directory path where the NRRD files will be saved.
+
+    Returns:
+    None
+    """
     convert_args_ct = {"input" :            rtstruct_path,
                         "output-prefix" :   nrrd_dir_path,
                         "prefix-format" :   'nrrd',
@@ -28,10 +64,22 @@ def convert_rtstruct_to_nrrd(rtstruct_path:str, nrrd_dir_path:str):
     pyplastimatch.convert(**convert_args_ct)
 
 def rigid_registration(fixed:sitk.Image, moving:sitk.Image, parameter)->Union[sitk.Image,sitk.Transform]:
+    """
+    Perform rigid registration between a fixed image and a moving image using the given parameter file.
+
+    Parameters:
+    fixed (sitk.Image): The fixed image to register.
+    moving (sitk.Image): The moving image to register.
+    parameter: The parameter file for the registration.
+
+    Returns:
+    Tuple[sitk.Image, sitk.Transform]: A tuple containing the registered image and the inverse transform.
+
+    """
+    
     # Perform registration based on parameter file
     elastixImageFilter = sitk.ElastixImageFilter()
     elastixImageFilter.SetParameterMap(parameter)
-    #elastixImageFilter.PrintParameterMap()
     elastixImageFilter.SetFixedImage(moving)  # due to FOV differences CT first registered to MR an inverted in the end
     elastixImageFilter.SetMovingImage(fixed)
     elastixImageFilter.LogToConsoleOn()
@@ -73,24 +121,53 @@ def rigid_registration(fixed:sitk.Image, moving:sitk.Image, parameter)->Union[si
     return registered_image,inverse_transform
 
 def correct_orientation(input_image:sitk.Image,order=[0,1,2],flip=[False,False,False]):
-    # Correct orientation of image, default is to do nothing
+    """
+    Corrects the orientation of an input image based on the specified order and flip parameters.
+
+    Parameters:
+    input_image (sitk.Image): The input image to be corrected.
+    order (list[int]): The order of axes for permuting the image. Default is [0, 1, 2].
+    flip (list[bool]): The flip status for each axis. Default is [False, False, False].
+
+    Returns:
+    sitk.Image: The corrected image with the specified orientation.
+    """
     image_permuted = sitk.PermuteAxes(input_image, order)
     image_flipped = sitk.Flip(image_permuted,flip)
     image_flipped.SetDirection([1,0,0,0,1,0,0,0,1])
     return image_flipped
 
-def nib_to_sitk(nib_image)->sitk.Image:
+def nib_to_sitk(nib_image) -> sitk.Image:
+    """
+    Convert a NIfTI image to a SimpleITK image.
+
+    Args:
+        nib_image: The NIfTI image to be converted.
+
+    Returns:
+        The converted SimpleITK image.
+    """
     img_nib_np = nib_image.get_fdata()
     nib_header = nib_image.header
-    img_nib_np = np.swapaxes(img_nib_np,0,2)
+    img_nib_np = np.swapaxes(img_nib_np, 0, 2)
     img_sitk = sitk.GetImageFromArray(img_nib_np)
-    img_sitk.SetSpacing((float(nib_header['pixdim'][1]),float(nib_header['pixdim'][2]),float(nib_header['pixdim'][3])))
-    img_sitk.SetOrigin((float(nib_header['srow_x'][3])*(-1),float(nib_header['srow_y'][3])*(-1),float(nib_header['srow_z'][3])))
-    img_sitk.SetDirection((1,0,0,0,1,0,0,0,1))
-    
+    img_sitk.SetSpacing((float(nib_header['pixdim'][1]), float(nib_header['pixdim'][2]), float(nib_header['pixdim'][3])))
+    img_sitk.SetOrigin((float(nib_header['srow_x'][3]) * (-1), float(nib_header['srow_y'][3]) * (-1), float(nib_header['srow_z'][3])))
+    img_sitk.SetDirection((1, 0, 0, 0, 1, 0, 0, 0, 1))
+
     return img_sitk
 
 def sitk_to_nib(sitk_image:sitk.Image):
+    """
+    Convert a SimpleITK image to a NIfTI image using nibabel.
+
+    Args:
+        sitk_image (sitk.Image): The SimpleITK image to be converted.
+
+    Returns:
+        nib.Nifti1Image: The converted NIfTI image.
+    """
+    
     def make_affine(simpleITKImage):
         # get affine transform in LPS
         c = [simpleITKImage.TransformContinuousIndexToPhysicalPoint(p)
@@ -116,6 +193,18 @@ def sitk_to_nib(sitk_image:sitk.Image):
     return img_nib
 
 def segment_defacing(ct_image:sitk.Image,structures=['brain','skull'])->Union[sitk.Image,sitk.Image]:
+    """
+    Generates brain and skull masks for defacing based on the ct image using totalsegmentator.
+
+    Args:
+        ct_image (sitk.Image): The CT image that should be defaced.
+        structures (list): The structures that should be segmented.
+
+    Returns:
+        sitk.Image: brain mask.
+        sitk.Image: skull mask.
+
+    """
     # Segment brain and skull from ct image for defacing using totalsegmentator
     ct_nib = sitk_to_nib(ct_image)
     segmentation = totalsegmentator(ct_nib,output=None,roi_subset=structures,quiet=True)
@@ -134,6 +223,17 @@ def segment_defacing(ct_image:sitk.Image,structures=['brain','skull'])->Union[si
     return brain,skull
 
 def defacing(brain_mask:sitk.Image, skull_mask:sitk.Image)->sitk.Image:
+    """
+    Applies defacing to the brain image based on the brain and skull masks.
+
+    Args:
+        brain_mask (sitk.Image): The brain mask image.
+        skull_mask (sitk.Image): The skull mask image.
+
+    Returns:
+        sitk.Image: The defaced brain image.
+
+    """
     # Create defacing mask based on brain and external ROI
     brain_mask_np = sitk.GetArrayFromImage(brain_mask)
     skull_mask_np = sitk.GetArrayFromImage(skull_mask)
